96cdfd9cc14c7f715e1ba919e843bf1bf914afa1
Riquegamer <hiquehique12348@gmail.com>
2023-10-25 19:42:50 -0300

    Privacy Policy 0.1

--- Assets/Scripts/MeteorSkin.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-[CreateAssetMenu(fileName = "Meteor", menuName = "Skins", order = 1)]
-public class MeteorSkin : ScriptableObject 
-{
-    public Sprite sprite; // Variavel responsavel pelo sprite principal da skin
-}
--- README.md
+++
@@ -1,2 +0,0 @@
-# Meteor_Scape
-Um jogo Android onde você é um astronalta que precisa desviar de meteoros através de giroscópio
